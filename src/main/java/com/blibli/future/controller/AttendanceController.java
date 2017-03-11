123e34743bf89593d14e31e5cb74e2dd2d523dd1
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-03-11 16:21:36 +0700

    uploadAttendanceFile on AttendanceController

--- src/main/java/com/blibli/future/controller/AttendanceController.java
+++ src/main/java/com/blibli/future/controller/AttendanceController.java
@@ -1,10 +1,26 @@
 package com.blibli.future.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class AttendanceController {
 
 
 
+    @PostMapping("/employees/taps/upload")
+    public ResponseEntity uploadAttendanceFile(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+
+        if (fileName.endsWith(".csv") || fileName.endsWith(".xslx")) {
+            //call soresponding service
+            return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<Boolean>(false, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
 }
