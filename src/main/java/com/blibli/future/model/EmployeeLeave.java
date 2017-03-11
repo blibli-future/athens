a72578b2dca68e987f9270249f5c22e48cecfeec
Sebastian Bagya Gunawan <sebastianbagyagunawan@gmail.com>
2017-03-11 16:10:35 +0700

    EmployeeLeave model and repository

---
+++ src/main/java/com/blibli/future/model/EmployeeLeave.java
@@ -0,0 +1,77 @@
+package com.blibli.future.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class EmployeeLeave {
+	
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	@Column(name = "id", unique = true)
+	private String id;
+	
+    private String nik;
+    private String idLeave;
+    private Date startDate;
+    private Date endDate;
+    private Date requestDate;
+    private String status;
+    private String reason;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNik() {
+		return nik;
+	}
+	public void setNik(String nik) {
+		this.nik = nik;
+	}
+	public String getIdLeave() {
+		return idLeave;
+	}
+	public void setIdLeave(String idLeave) {
+		this.idLeave = idLeave;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Date getRequestDate() {
+		return requestDate;
+	}
+	public void setRequestDate(Date requestDate) {
+		this.requestDate = requestDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+    
+}
