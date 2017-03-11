413239febe473ca176fba0d7da67c5317284bbd0
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-03-11 11:42:20 +0700

    Initial Commit

---
+++ src/main/java/com/blibli/future/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.blibli.future;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AthensApplication.class);
+	}
+
+}
