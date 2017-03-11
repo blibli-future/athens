528e8d03ae074e750738b8a472e472395b9a93d6
salsa <azhalia.amesa@si.ukdw.ac.id>
2017-03-11 16:25:57 +0700

    Add attendance model and repository

---
+++ src/main/java/com/blibli/future/model/Attendance.java
@@ -0,0 +1,50 @@
+
+package com.blibli.future.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Attendance {
+    @Id
+    private String nik;
+    private Timestamp tapIn;
+    private Timestamp tapOut;
+    private Date date;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public Timestamp getTapIn() {
+        return tapIn;
+    }
+
+    public void setTapIn(Timestamp tapIn) {
+        this.tapIn = tapIn;
+    }
+
+    public Timestamp getTapOut() {
+        return tapOut;
+    }
+
+    public void setTapOut(Timestamp tapOut) {
+        this.tapOut = tapOut;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+}
---
+++ src/main/java/com/blibli/future/repository/AttendanceRepository.java
@@ -0,0 +1,11 @@
+
+package com.blibli.future.repository;
+
+import com.blibli.future.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, String>{
+    
+}
