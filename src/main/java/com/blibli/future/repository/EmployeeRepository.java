ce3f51d5b074276c0f18e267bb5a07fb1d6e821a
salsa <azhalia.amesa@si.ukdw.ac.id>
2017-03-11 15:38:41 +0700

    Add employee repository

---
+++ src/main/java/com/blibli/future/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+
+package com.blibli.future.repository;
+
+import com.blibli.future.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    
+}
