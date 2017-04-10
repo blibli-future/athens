8f7c3c3d0214e6abdc31089371a74e50423c0289
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-04-10 14:46:48 +0700

    Add more test case to attendance model

--- src/test/java/com/blibli/future/model/AttendanceTest.java
+++ src/test/java/com/blibli/future/model/AttendanceTest.java
@@ -8,29 +8,56 @@ import java.time.LocalTime;
 
 public class AttendanceTest {
     private static LocalDate TAP_DATE = LocalDate.of(2017, 4, 1);
+    private static LocalTime EARLY_TIME = LocalTime.of(10, 0, 0);
+    private static LocalTime MIDDLE_TIME = LocalTime.of(12, 0, 0);
+    private static LocalTime LATE_TIME = LocalTime.of(19, 0, 0);
     private static String NIK = "abc";
     @Test
     public void assignTest_NoTapOutNewTapAfterTapIn() {
-        LocalTime tapIn = LocalTime.of(9, 0, 0);
-        LocalTime tapOut = LocalTime.of(21, 0, 0);
+        Attendance expected = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+        Attendance actual = new Attendance(NIK, TAP_DATE, LATE_TIME);
 
-        Attendance expected = new Attendance(NIK, TAP_DATE, tapIn, tapOut);
-        Attendance actual = new Attendance(NIK, TAP_DATE, tapIn);
-
-        actual.assign(tapOut);
+        actual.assign(EARLY_TIME);
 
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void assignTest_NoTapOutNewTapBeforeTapIn() {
-        LocalTime tapIn = LocalTime.of(10, 0, 0);
-        LocalTime tapOut = LocalTime.of(19, 0, 0);
+        Attendance expected = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+        Attendance actual = new Attendance(NIK, TAP_DATE, EARLY_TIME);
 
-        Attendance expected = new Attendance(NIK, TAP_DATE, tapIn, tapOut);
-        Attendance actual = new Attendance(NIK, TAP_DATE, tapOut);
+        actual.assign(LATE_TIME);
 
-        actual.assign(tapIn);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void assignTest_newTapBeforeTapIn() {
+        Attendance expected = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+        Attendance actual = new Attendance(NIK, TAP_DATE, MIDDLE_TIME, LATE_TIME);
+
+        actual.assign(EARLY_TIME);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void assignTest_newTapBetweenTap() {
+        Attendance expected = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+        Attendance actual = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+
+        actual.assign(MIDDLE_TIME);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void assignTest_newTapAfterTapIn() {
+        Attendance expected = new Attendance(NIK, TAP_DATE, EARLY_TIME, LATE_TIME);
+        Attendance actual = new Attendance(NIK, TAP_DATE, EARLY_TIME, MIDDLE_TIME);
+
+        actual.assign(LATE_TIME);
 
         Assert.assertEquals(expected, actual);
     }
