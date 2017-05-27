cc77ce5c10b4dda1b310bf8743c371159a275f5f
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-05-27 20:04:22 +0700

    Change autowired to Dozer singleton

--- src/main/java/com/blibli/future/service/ConverterServiceImpl.java
+++ src/main/java/com/blibli/future/service/ConverterServiceImpl.java
@@ -1,17 +1,15 @@
 package com.blibli.future.service;
 
-import org.dozer.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.blibli.future.service.api.ConverterService;
+import org.dozer.DozerBeanMapperSingletonWrapper;
+import org.dozer.Mapper;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ConverterServiceImpl implements ConverterService {
-	  @Autowired
-	  private Mapper mapper;
-	  
-	  @Override
+	private Mapper mapper = DozerBeanMapperSingletonWrapper.getInstance();
+
+	@Override
 	  public <S, T> T map(S source, Class<T> destinationClass) {
 	    return source == null ? null : this.mapper.map(source, destinationClass);
 	  }
