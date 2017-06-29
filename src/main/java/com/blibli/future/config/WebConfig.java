3ce45872c01ac2305489582f47e648b2404d4e6f
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-06-30 03:52:28 +0700

    Add swagger support

--- src/main/java/com/blibli/future/config/WebConfig.java
+++ src/main/java/com/blibli/future/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.blibli.future.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -13,5 +14,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
