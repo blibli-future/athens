adcf246aef50f86b4bc314ca2f463fa74bdd89d2
Sebastian Bagya Gunawan <sebastianbagyagunawan@gmail.com>
2017-07-22 21:57:21 +0700

    EmployeeController

--- src/main/java/com/blibli/future/service/EmployeeServiceImpl.java
+++ src/main/java/com/blibli/future/service/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import com.blibli.future.enums.Religion;
 import com.blibli.future.model.Employee;
 import com.blibli.future.repository.EmployeeRepository;
 import com.blibli.future.service.api.EmployeeService;
+import com.blibli.future.vo.EmployeeVo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,39 +13,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by amesa on 3/21/17.
- */
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
-
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    
     @Autowired
     public EmployeeServiceImpl (EmployeeRepository employeeRepository){
         this.employeeRepository = employeeRepository;
     }
 
-
-
-    public boolean saveEmployee (String nik, String fullName, Gender gender, String position, String level, String organizationalUnitText,
-                                 MaritalStatus maritalStatus, Religion religion, String nameOfDept, String chiefNik, String chiefName, String chiefPosition,
-                                 String chiefPositionText, LocalDate startWorkingDate, LocalDate endWorkingDate, Boolean status){
-        if(isEmployeeExist(nik)){
-            return false;
+    public Employee saveEmployee (EmployeeVo employeeVo){
+        if(isEmployeeExist(employeeVo.getNik())){
+            return null;
         }else{
-            Employee emp = new Employee(nik, fullName,gender,position,level,
-                    organizationalUnitText,maritalStatus,religion,nameOfDept,chiefNik,
-                    chiefName,chiefPosition,chiefPositionText,
-                    startWorkingDate,endWorkingDate,status);
+            Employee emp = new Employee(employeeVo.getNik(), employeeVo.getFullName(), Gender.valueOf(employeeVo.getGender()), employeeVo.getPosition(), 
+            		employeeVo.getLevel(), employeeVo.getOrganizationalUnitText(), MaritalStatus.valueOf(employeeVo.getMaritalStatus()), Religion.valueOf(employeeVo.getReligion()), employeeVo.getNameOfDept(),
+            		employeeVo.getChiefNik(), employeeVo.getChiefName(), employeeVo.getChiefPosition(), employeeVo.getChiefPositionText(),
+            		LocalDate.parse(employeeVo.getStartWorkingDate(), formatter), LocalDate.parse(employeeVo.getEndWorkingDate(), formatter), employeeVo.getStatus());
             employeeRepository.save(emp);
-            return true;
+            return emp;
         }
-
-
     }
     public boolean isEmployeeExist(String nik){
         if (employeeRepository.findOneByNik(nik) !=null){
@@ -54,29 +47,27 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
 
-    public boolean updateEmployee (String nik, String fullName, Gender gender, String position, String level, String organizationalUnitText,
-            MaritalStatus maritalStatus, Religion religion, String nameOfDept, String chiefNik, String chiefName, String chiefPosition,
-            String chiefPositionText, LocalDate startWorkingDate, LocalDate endWorkingDate, Boolean status){
-    		Employee oldEmployee = employeeRepository.findOneByNik(nik);
+    public Employee updateEmployee (EmployeeVo employeeVo){
+    		Employee oldEmployee = employeeRepository.findOneByNik(employeeVo.getNik());
     	if(oldEmployee!=null){
-    	   oldEmployee.setChiefName(chiefName);
-    	   oldEmployee.setChiefNik(chiefNik);
-    	   oldEmployee.setChiefPosition(chiefPosition);
-    	   oldEmployee.setChiefPositionText(chiefPositionText);
-    	   oldEmployee.setEndWorkingDate(endWorkingDate);
-    	   oldEmployee.setFullName(fullName);
-    	   oldEmployee.setGender(gender);
-    	   oldEmployee.setLevel(level);
-    	   oldEmployee.setMaritalStatus(maritalStatus);
-    	   oldEmployee.setNameOfDept(nameOfDept);
-    	   oldEmployee.setOrganizationalUnitText(organizationalUnitText);
-    	   oldEmployee.setReligion(religion);
-    	   oldEmployee.setStartWorkingDate(startWorkingDate);
-    	   oldEmployee.setStatus(status);
+    	   oldEmployee.setChiefName(employeeVo.getChiefName());
+    	   oldEmployee.setChiefNik(employeeVo.getChiefNik());
+    	   oldEmployee.setChiefPosition(employeeVo.getChiefPosition());
+    	   oldEmployee.setChiefPositionText(employeeVo.getChiefPositionText());
+    	   oldEmployee.setEndWorkingDate(LocalDate.parse(employeeVo.getEndWorkingDate(), formatter));
+    	   oldEmployee.setFullName(employeeVo.getFullName());
+    	   oldEmployee.setGender(Gender.valueOf(employeeVo.getGender()));
+    	   oldEmployee.setLevel(employeeVo.getLevel());
+    	   oldEmployee.setMaritalStatus(MaritalStatus.valueOf(employeeVo.getMaritalStatus()));
+    	   oldEmployee.setNameOfDept(employeeVo.getNameOfDept());
+    	   oldEmployee.setOrganizationalUnitText(employeeVo.getOrganizationalUnitText());
+    	   oldEmployee.setReligion(Religion.valueOf(employeeVo.getReligion()));
+    	   oldEmployee.setStartWorkingDate(LocalDate.parse(employeeVo.getStartWorkingDate(), formatter));
+    	   oldEmployee.setStatus(employeeVo.getStatus());
            employeeRepository.save(oldEmployee);
-           return true;
+           return oldEmployee;
        }
-       return false;
+       return null;
     }
 
 
@@ -87,7 +78,16 @@ public class EmployeeServiceImpl implements EmployeeService {
             return listEmployee;
         }
         return null;
-
     }
+    
+	@Override
+	public List<Employee> getAllEmployees() {
+		List<Employee> listEmployee = new ArrayList<>();
+        listEmployee = employeeRepository.findAll();
+        if(listEmployee!=null){
+            return listEmployee;
+        }
+        return null;
+	}
 
 }
