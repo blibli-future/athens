5c2f5b6ea05264f6216c6c592ec40d2c20d0005a
Sebastian Bagya Gunawan <sebastianbagyagunawan@gmail.com>
2017-07-24 13:40:49 +0700

    edit EmployeeController

--- src/main/java/com/blibli/future/controller/EmployeesController.java
+++ src/main/java/com/blibli/future/controller/EmployeesController.java
@@ -2,13 +2,13 @@ package com.blibli.future.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blibli.future.model.Employee;
@@ -19,7 +19,11 @@ import com.blibli.future.vo.EmployeeVo;
 public class EmployeesController {
 	public static final String BASE_PATH = "/employees";
 	private EmployeeService employeeService;
-	EmployeesController(){}
+	
+	@Autowired
+	public EmployeesController(EmployeeService employeeService){
+		this.employeeService = employeeService;
+	}
 	
 	@PostMapping(BASE_PATH)
     public ResponseEntity<Employee> Employee(@RequestBody EmployeeVo employeeVo){
@@ -33,7 +37,7 @@ public class EmployeesController {
     }
 	
     @GetMapping(BASE_PATH)
-    public ResponseEntity<List<Employee>> employeeGetByDepartment(){
+    public ResponseEntity<List<Employee>> employeeGetAll(){
         List<Employee> getEmployees =
                 employeeService.getAllEmployees();
         if(getEmployees!= null){
