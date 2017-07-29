68be81ef78156d991a3ef645e14eb988508402a9
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-07-29 16:41:43 +0700

    Add fail test case for EmployeeStatisticServiceImpl

--- src/main/java/com/blibli/future/vo/SummariesVo.java
+++ src/main/java/com/blibli/future/vo/SummariesVo.java
@@ -65,4 +65,32 @@ public class SummariesVo {
     public void setMaxEmployeeLate(int maxEmployeeLate) {
         this.maxEmployeeLate = maxEmployeeLate;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        SummariesVo that = (SummariesVo) o;
+
+        if(yearlyLeaveUsed != that.yearlyLeaveUsed) return false;
+        if(substitutionLeaveRightUsed != that.substitutionLeaveRightUsed) return false;
+        if(lateUsed != that.lateUsed) return false;
+        if(maxYearlyLeave != that.maxYearlyLeave) return false;
+        if(maxSubstitutionLeaveRight != that.maxSubstitutionLeaveRight) return false;
+        if(maxEmployeeLate != that.maxEmployeeLate) return false;
+        return nik.equals(that.nik);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nik.hashCode();
+        result = 31 * result + (int) (yearlyLeaveUsed ^ (yearlyLeaveUsed >>> 32));
+        result = 31 * result + (int) (substitutionLeaveRightUsed ^ (substitutionLeaveRightUsed >>> 32));
+        result = 31 * result + (int) (lateUsed ^ (lateUsed >>> 32));
+        result = 31 * result + maxYearlyLeave;
+        result = 31 * result + maxSubstitutionLeaveRight;
+        result = 31 * result + maxEmployeeLate;
+        return result;
+    }
 }
--- src/test/java/com/blibli/future/service/EmployeeStatisticServiceImplTest.java
+++ src/test/java/com/blibli/future/service/EmployeeStatisticServiceImplTest.java
@@ -1,11 +1,16 @@
 package com.blibli.future.service;
 
+import com.blibli.future.exception.IdNotFoundException;
 import com.blibli.future.repository.*;
+import org.hamcrest.Matchers;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class EmployeeStatisticServiceImplTest {
     @InjectMocks
@@ -21,18 +26,39 @@ public class EmployeeStatisticServiceImplTest {
     @Mock
     private SubstitutionLeaveRightRepository substitutionLeaveRightRepository;
 
-    @Test
-    public void generateSummaries() throws Exception {
+    private final String NIK = "nik";
 
+    @Test
+    public void generateSummaries_fail() throws Exception {
+        Mockito.when(employeeRepository.findOneByNik(NIK)).thenReturn(null);
+
+        try {
+            employeeStatisticService.generateSummaries(NIK);
+        } catch (Exception e) {
+            Assert.assertThat(e, Matchers.instanceOf(IdNotFoundException.class));
+        }
+
+        Mockito.verify(employeeRepository).findOneByNik(NIK);
+    }
+
+    @Test
+    public void generateSummaries_success() throws Exception {
+        //How to test this?
     }
 
     @Before
     public void setUp() throws Exception {
-
+        MockitoAnnotations.initMocks(this);
     }
 
     @After
     public void tearDown() throws Exception {
+        Mockito.verifyNoMoreInteractions(
+                employeeRepository,
+                employeeYearlyLeaveRepository,
+                employeeSubstitutionLeaveRightRepository,
+                attendanceRepository,
+                substitutionLeaveRightRepository);
 
     }
 }
