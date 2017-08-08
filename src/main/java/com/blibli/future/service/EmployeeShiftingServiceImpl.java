e244eaca3f89299b20ec2a68c08b7ea8d1798fdc
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-08-09 01:20:25 +0700

    Activate Shifting: Remove unused method from EmployeeShifting service and other class affected by change

--- src/main/java/com/blibli/future/service/EmployeeShiftingServiceImpl.java
+++ src/main/java/com/blibli/future/service/EmployeeShiftingServiceImpl.java
@@ -9,8 +9,6 @@ import com.blibli.future.service.api.EmployeeShiftingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 public class EmployeeShiftingServiceImpl implements EmployeeShiftingService{
 	private final EmployeeRepository employeeRepository;
@@ -38,33 +36,6 @@ public class EmployeeShiftingServiceImpl implements EmployeeShiftingService{
 		return null;
 	}
 
-	@Override
-	public Employee employeeShiftingUpdate(String nik, String newShift, String oldShift) {
-		Employee emp = employeeRepository.findOneByNik(nik);
-		Shift shiftOld = shiftRepository.findOneById(oldShift);
-		Shift shiftNew = shiftRepository.findOneById(newShift);
-		if(emp!=null&&shiftOld!=null&&shiftNew!=null){
-			emp.deleteShifts(shiftOld);
-			emp.addShifts(shiftNew);
-			Employee shiftUpdated = employeeRepository.save(emp);
-			if(shiftUpdated!=null)
-				return shiftUpdated;
-			//Log update failed dudeh
-			return null;
-		}
-		//Log emp newshift or oldshift null dudeh
-		return null;
-	}
-
-	@Override
-	public Set<Employee> processGetShifting(String idShift) {
-		Shift shift = shiftRepository.findOneById(idShift);
-		if(shift!=null){
-			return shift.getEmployees();
-		}
-		//Log shift not found dudeh
-		return null;
-	}
 
     @Override//TODO: what should it return?
     public void removeShiftFromEmployee(String shiftId, String nik) throws IdNotFoundException {
--- src/main/java/com/blibli/future/service/api/EmployeeShiftingService.java
+++ src/main/java/com/blibli/future/service/api/EmployeeShiftingService.java
@@ -3,12 +3,8 @@ package com.blibli.future.service.api;
 import com.blibli.future.exception.IdNotFoundException;
 import com.blibli.future.model.Employee;
 
-import java.util.Set;
-
 public interface EmployeeShiftingService {
 	Employee assignShiftToEmployee(String shiftId, String nik);
-	Employee employeeShiftingUpdate(String nik, String newShift, String oldShift);
-	Set<Employee> processGetShifting(String idShift);
-
+	
 	void removeShiftFromEmployee(String shiftId, String nik) throws IdNotFoundException;
 }
